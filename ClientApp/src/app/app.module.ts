75fb0d39c8102dec1d81964116420d2d3e52aa44
leilahsmith <leilah.sdev@gmail.com>
2023-03-26 19:34:17 -0300

    Formatting Changes

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -15,6 +15,16 @@ import { CertificationsComponent } from './certifications/certifications.compone
 import { SkillsComponent } from './skills/skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { CoursesComponent } from './courses/courses.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  }
+];
 
 @NgModule({
   declarations: [
@@ -28,18 +38,29 @@ import { CoursesComponent } from './courses/courses.component';
     CertificationsComponent,
     SkillsComponent,
     PortfolioComponent,
-    CoursesComponent
+    CoursesComponent,
+    ResumeComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      { path: '', component: HomeComponent },
+      { path: 'hero', component: HeroComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'certifications', component: CertificationsComponent },
+      { path: 'portfolio', component: PortfolioComponent },
+      { path: 'skills', component: SkillsComponent },
+      { path: 'courses', component: CoursesComponent },
+      { path: 'resume', component: ResumeComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: 'footer', component: FooterComponent },
     ])
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
